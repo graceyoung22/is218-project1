2ccd7944a04f5b641e525209a54916bd93d68bf4
Grace A Young <gay5@afsconnect1.njit.edu>
2016-09-29 14:25:21 -0400

    more1 class code

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-//This is how you print text in php
+//This is how you print text in php;
 echo 'hello world';
-//This is how you store a value in a variable
+//This is how you store a value in a variable;
 $myvar = 'Some text I want to store in a variable';
 
-//This is an example of the difference between single quotes and double quotes
+//This is an example of the difference between single quotes and double quotes;
 echo '$myvar';
 echo '<br>';
 echo "$myvar";
 
-//This is an example of php arrays
+//This is an example of php arrays;
 
 $myarray = array();
 $myarray[] = 'some value 1';
@@ -18,29 +18,38 @@ $myarray[] = 'some value 2';
 $myarray[] = 'some value 3';
 
 print_r($myarray);
-//This is an example of an associative array and a nested array
+//This is an example of an associative array and a nested array;
 $myAssoc = array('value1' => $myarray, 'value2' => $myarray);
 
 print_r($myAssoc);
-//This is how you print r/ access an array by naming the key you want to acess
+//This is how you print r/ access an array by naming the key you want to acess;
 print_r($myAssoc['value1']);
 
-//This is how you define a class in php
+//This is how you define a class in php;
 class myclass{
 public $myPublic;
 private $myPrivate;
 protected $myProtected;
 
 public fuction __construct() {
+//This is the internal reference to the object within a class, the arrow points to a property or method being;
+//accessed within the oject;
 
 $this->myPublic = 1;
 $this->myPrivate = 2;
 $this->myProtected = 3;
+//This is how you all a method inside an object and pass a value;
+$this->sayHello('Grace');
 }
+public function sayHello ($name) {
+echo 'Hello ' . $name;
 
 }
 
-//This is how you in
+
+}
+
+//This is how you in;
 $obj = new myclass;
 
 print_r($obj);
